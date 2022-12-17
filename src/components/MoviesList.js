7b3664e5d844181f11c8ed8b337b9ee7ed2b89e7
Anju Vishwakarma <111855561+0123anjuvish@users.noreply.github.com>
2022-12-17 16:56:05 +0530

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,3 +4,37 @@ import moviesList from "./MovieContext";
 class MoviesList extends React.Component {
 
   render() {
+    
+    // Get movies list using MoviesContext.
+
+    return (
+      <div id="movies-table">
+        <style>{
+        `
+        td { padding : 20px; }
+        tr:hover { background: #3374C2}
+        `}</style>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+             
+              {moviesList.map((val)=>{
+                return(
+                  <tr id="table-row" key={val.id}>
+                <td onClick={()=>this.props.function(val)} >{val.title}</td>
+                </tr>
+                )
+              })}
+              
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
