90e62a176cf3b5871af7148770155b103b902f39
Anju Vishwakarma <111855561+0123anjuvish@users.noreply.github.com>
2022-12-17 16:53:14 +0530

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,33 +1,6 @@
 import React from "react";
+import moviesList from "./MovieContext";
 
 class MoviesList extends React.Component {
 
   render() {
-    
-    // Get movies list using MoviesContext.
-
-    return (
-      <div id="movies-table">
-        <style>{
-        `
-        td { padding : 20px; }
-        tr:hover { background: #3374C2}
-        `}</style>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-            </tr>
-          </thead>
-          <tbody>
-              <tr id="table-row">
-                <td>Movie Title</td>
-              </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
-
-export default MoviesList;
